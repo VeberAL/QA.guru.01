ecc57d82c3d9ef2474371aaa3fd35f4401a76174
Alexander Veber <aleo83@rambler.ru>
2024-02-05 19:03:35 +0500

    Update SearchTest.java

--- src.main/src/test/java/SearchTest.java
+++ src.main/src/test/java/SearchTest.java
@@ -2,13 +2,13 @@ import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.opens;
 
 public class SearchTest {
     @Test
     void successfulSearchTest() {
-        open("https://www.google.com/121");
+        open("https://www.google.com/67");
         $("[name=q]").setValue("selenide").pressEnter();
-        $("[id=search]").shouldHave(text("https://selenide.org"));
+        $("[id=search]").shouldHave(text("https://selenide"));
     }
 }
